9bc3eaf44c7366f7e7630cd8cdc7d000ded90ac2
ajtwu1235 <75232182+ajtwu1235@users.noreply.github.com>
2021-03-22 17:08:52 +0900

    Crain_Doll_Draw.java
    크레인 뽑기 게임

--- Crain_Doll_Draw.java
+++ Crain_Doll_Draw.java
@@ -40,7 +40,7 @@ public class Crain_Doll_Draw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] board= {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-		int[] moves = {1,5,3,5,1,2,1,4};
+		int[] moves = {1,5,3,5,1,2,1,4,5};
 		System.out.print(solution(board,moves));
 	}
 
